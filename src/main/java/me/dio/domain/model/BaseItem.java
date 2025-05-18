c23f5769a67f91863fb04b07b3d341a99c11782e
flaviane-lima <rflaviane41@gmail.com>
2025-05-18 20:20:02 -0300

    Primeira tentativa de publicação no Railway

--- src/main/java/me/dio/domain/model/BaseItem.java
+++ src/main/java/me/dio/domain/model/BaseItem.java
@@ -1,9 +1,6 @@
 package me.dio.domain.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 
 @MappedSuperclass
 public abstract class  BaseItem {
